f798ac6b640568e66e52e815d60d6c8182000579
sam <evaoosyz@gmail.com>
2017-01-14 23:11:07 +0800

    A01_Generics 完成

--- CSharpLab/Program.cs
+++ CSharpLab/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpLab.A01_Generics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,19 @@ namespace CSharpLab
     {
         static void Main(string[] args)
         {
+            var a = new APIData<Product, int>()
+            {
+                Version = "V1",
+                ErrorCode = 0,
+                Data = new Product()
+                {
+                    ID = 87,
+                    Name = "產品"
+                }
+            };
+            var cc = JsonHelper.ToJson(a);
+
+        var c = JsonHelper.JsonToObject<APIData<Product,int>>("a");
         }
     }
 }
